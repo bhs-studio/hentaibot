4e2ccea9ca0ba112018c91b4eea4cfc274a7e4ac
User Unknown <aveBHS.ru@yandex.ru>
2020-02-28 17:30:38 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,2 +1,6 @@
 token = "" #TODO your token
 groupID = "" #TODO your group id
+serverdb = "127.0.0.1" #MySQL server adress
+dbname = "" #MySQL database name
+userdb = "root" #MySQL user name
+passworddb = "" #MySQL user password
